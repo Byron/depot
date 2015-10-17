540c2411f6ce32041a0f2fec1e2eaeaf4cd3b018
Sebastian Thiel <byronimo@gmail.com>
2015-10-17 19:24:52 +0200

    feat(websies): configuration file to allow uploads

---
+++ src/bash/websies.sh
@@ -0,0 +1,8 @@
+
+# Let's build a youtube uploader
+playlist_id=PLMHbQxe1e9Mkceo3tCX2ken78WbvM-0R-
+# the repository to which to upload by default
+default_repo=katars
+
+github_user=byron
+privacy=public
